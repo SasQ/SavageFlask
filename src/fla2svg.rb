bdb2bb37efa72ccf978b35a97b25840e8374ad11
SasQ <sasq1@go2.pl>
2013-09-02 14:09:20 +0200

    Added functions for accessing parts of a command and for simplifying edge data by removing redundant `moveTo`.

--- src/fla2svg.rb
+++ src/fla2svg.rb
@@ -28,19 +28,13 @@ edges.each { |edge| p edge }
 
 
 # For starters, let's just read the edge data into a nested array of commands.
-# Each item of this array has the format: `[ command, *parameters ]`
-# where `parameters` are just coordinate numbers.
-# We will also use human-readable names (Ruby symbols for efficiency) for the commands.
+# Each command is an array of the form: `[ opcode, *parameters ]`
+# where `parameters` is just a series of coordinate numbers.
+# We will also use Ruby symbols for the opcodes, for better readability and efficiency.
 
-edgeData = edges[0]['edges']
-
-puts "\nConverting this:"
-p edgeData
-
-
-# Converts XFL opcodes to human-readable symbols.
-def cmdToSym(cmdstr)
-	return { '!' => :moveTo, '|' => :lineTo, '/' => :lineTo, '[' => :curveTo, '(' => :cubicTo }[cmdstr]
+# Convert XFL opcodes to human-readable symbols.
+def opStrToSym(opcodeStr)
+	return { '!' => :moveTo, '|' => :lineTo, '/' => :lineTo, '[' => :curveTo, '(' => :cubicTo }[opcodeStr]
 end
 
 # Converts 32-bit fixed-point number from hexadecimal string to its corresponding number.
@@ -51,39 +45,76 @@ def hexToNum(hexstr)
 	num.to_f / 256
 end
 
-# Reads XFL edge data string into a nested array.
-# Numbers are already decoded as floating point values with correct scale.
+# Read XFL edge data string into a nested array.
+# Numbers are decoded as floating point values with correct scale.
 def edgeData2arr(edgeData)
-	edgeData.scan( /([!|\/\[])([^!|\/\[]+)/ ).map do |cmd,ops|
-		numbers = ops.scan( /(#?)(\S+)\s*/ ).map do |prefix,num|
-			prefix == '#'  ?  hexToNum(num)  :  num.to_f / 20
+	edgeData.scan( /([!|\/\[])([^!|\/\[]+)/ ).map do |opcode,params|
+		numbers = params.scan( /(#?)(\S+)\s*/ ).map do |prefix,number|
+			prefix == '#'  ?  hexToNum(number)  :  number.to_f / 20
 		end
-		[ cmdToSym(cmd), *numbers ]
+		[ opStrToSym(opcode), *numbers ]
 	end
 end
+
+
+# Let's try it on the first edge.
+edgeData = edges[0]['edges']
+
+puts "\nConverting this:"
+p edgeData
 
 puts "\ninto this:"
 edgeArr = edgeData2arr(edgeData)
 p edgeArr
 
 
+# Simplify the path data.
+
+# Return the command's opcode.
+def cmdOp(cmd)
+	return cmd[0]  # First item is the opcode.
+end
+
+# Return the command's destination point.
+def cmdTo(cmd)
+	return cmd[-2..-1]  # Last two numbers are always the endpoint.
+end
+
+# TODO: If we make command to be a class, the above functions can be then turned into its methods.
+
+# Simplify the edge data:
+# If a `moveTo` command has the same coordinates as the endpoint of the preceding command,
+# it is redundant and can be safely removed.
+def simplifyPath!(edgeArr)
+	edgeArr.each_with_index do |cmd,index|
+		next unless cmdOp(cmd) == :moveTo or index > 0
+		prevCmd = edgeArr[index-1]
+		edgeArr.delete_at(index) if cmdTo(prevCmd) == cmdTo(cmd)
+	end
+end
+
+
+simplifyPath!(edgeArr)
+
+
 # Let's try to spit it out as SVG path.
 
-# Converts the command symbol to SVG vector command.
-def cmdSymToSVG(cmdsym)
-	return { :moveTo => 'M', :lineTo => 'L', :curveTo => 'Q', :cubicTo => 'C' }[cmdsym]
+# Convert the opcode symbol to SVG vector command.
+def opSymToSVG(sym)
+	return { :moveTo => 'M', :lineTo => 'L', :curveTo => 'Q', :cubicTo => 'C' }[sym]
 end
 
 # Create an SVG path element from the given array with edge data.
 def SVG_path(edgeArr)
 	pathElem = XML::Node.new('path')
-	pathElem['d'] = edgeArr.map { |cmd,*ops| [ cmdSymToSVG(cmd), *ops ] }.join(' ')
+	pathElem['d'] = edgeArr.map { |opcode,*params| [ opSymToSVG(opcode), *params ] }.join(' ')
 	return pathElem
 end
+
 
 puts "\nConverting to SVG path:"
 pathElem = SVG_path(edgeArr)
 p pathElem
 
 
-# TODO: Next step: Simplify the SVG path from redundant moveTo's.
+# TODO: Next step: Modularization and cleaning up.
